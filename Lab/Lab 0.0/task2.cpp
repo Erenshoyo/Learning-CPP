8d41e122a1c75de6f162c9c636bb94b9dd7f663a
Md. Asif Shahriar Tauhid <70537695+Erenshoyo@users.noreply.github.com>
2025-05-25 22:04:41 +0600

    Lab works added

---
+++ Lab/Lab 0.0/task2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+long long factorial(int n){
+    if(n <= 1) return 1;
+    return n * factorial(n-1);
+}
+int main() {
+    int n;
+    cout << "Enter the number to calculate factorial: ";
+    cin >> n;
+
+    if( n < 0){
+        cout << "Factorial is not defined for negative numbers."<< endl; 
+    }
+    else{
+        cout << "Factorial of " << n << " is " << factorial(n) << endl;
+    }
+
+    return 0;
+}
